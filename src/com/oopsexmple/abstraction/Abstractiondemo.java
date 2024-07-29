2c240c694099bff40c4f5a267e110eafe871720d
Akash Ghatage. <87923828+AKASHGHATAGE@users.noreply.github.com>
2024-07-30 00:09:05 +0530

    update on remote repository

--- src/com/oopsexmple/abstraction/Abstractiondemo.java
+++ src/com/oopsexmple/abstraction/Abstractiondemo.java
@@ -5,6 +5,7 @@ public class Abstractiondemo {
 	public static void main(String[] args) {
 		
 		System.out.println("HI Java developers");
+		System.out.println("Updadtin on remorte repository ");
 	}
 
 }
